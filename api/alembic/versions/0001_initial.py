0f9ace6dd3ce46cb8dd2db0b762775de6eb90b08
Gedion08 <gedions808@gmail.com>
2026-08-17 16:18:11 +0300

    make the initial migration's revision match what 0002 expects.

--- api/alembic/versions/0001_initial.py
+++ api/alembic/versions/0001_initial.py
@@ -10,7 +10,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision: str = "0001_initial"
+revision: str = "0001"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
